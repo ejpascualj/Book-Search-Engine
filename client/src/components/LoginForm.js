012391de52f68c23ea812311f300cfbcdcdc89ad
Emilio P <ejpascualj@gmail.com>
2021-08-10 01:05:31 -0500

    modified LoginForm.js

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
 // import { loginUser } from '../utils/API';
-import { useMutation } from '@apollo/react-hooks';
+import { useMutations } from '@apollo/react-hooks';
 import { LOGIN_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
 
