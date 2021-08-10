c432b035b9b896fea629851d4de3baf09c2c3633
Emilio P <ejpascualj@gmail.com>
2021-08-10 08:49:31 -0500

    fixed mistake

--- server/server.js
+++ server/server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 // const routes = require('./routes');
-const {ApolloServer} = require('apollo-server-express');
-const { typeDefs, resolvers} = require('/schemas');
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers} = require('./schemas');
 const {authMiddleware} = require('./utils/auth');
 
 const app = express();
