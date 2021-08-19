6109d8a53015938f3637dc3389e29c5d763c4d45
Emilio P <ejpascualj@gmail.com>
2021-08-19 00:26:30 -0500

    modified queries.js

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,17 +1,37 @@
-import gql from 'graphql-tag';
-export const QUERY_ME = gql`
+import {gql} from '@apollo/client';
+
+export const QUERY_PROFILES = gql`
+  query allProfiles {
+    profiles {
+      _id
+      name
+      skills
+    }
+  }
+`;
+
+export const QUERY_SINGLE_PROFILE = gql`
+  query singleProfile($profileId: ID!) {
+    profile(profileId: $profileId) {
+      _id
+      name
+      skills
+    }
+  }
+`;
+
+export const GET_ME = gql`
     {
         me {
             _id
             username
             email
             savedBooks {
+                title
                 bookId
                 authors
-                image
                 description
-                title
-                link
+                image
             }
         }
     }
