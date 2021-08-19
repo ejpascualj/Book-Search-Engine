842173a522a7bebbfde1a152d071dcaf01e1b9a4
Emilio P <ejpascualj@gmail.com>
2021-08-19 17:40:00 -0500

    modified utils

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,16 +1,4 @@
-import gql from "graphql-tag";
-
-export const LOGIN_USER = gql`
-  mutation login($email: String!, $password: String!) {
-    login(email: $email, password: $password) {
-      token
-      user {
-        _id
-        username
-      }
-    }
-  }
-`;
+import { gql } from '@apollo/client';
 
 export const ADD_USER = gql`
   mutation addUser($username: String!, $email: String!, $password: String!) {
@@ -24,37 +12,49 @@ export const ADD_USER = gql`
   }
 `;
 
+
 export const SAVE_BOOK = gql`
-  mutation saveBook($bookData: BookInput!) {
-    saveBook(bookData: $bookData) {
-      _id
+  mutation saveBook($input: BookInput) {
+    saveBook(input: $input) {
       username
-      email
+      _id
+      bookCount
       savedBooks {
         bookId
         authors
         image
-        description
         title
-        link
+        description
       }
     }
   }
 `;
 
+export const LOGIN_USER = gql`
+  mutation loginUser($email: String!, $password: String!) {
+    loginUser(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+
 export const REMOVE_BOOK = gql`
-  mutation removeBook($bookId: ID!) {
-    removeBook(bookId: $bookId) {
-      _id
-      username
-      email
+mutation deleteBook($bookId: String!) {
+  deleteBook(bookId: $bookId) {
+    _id
+    username
+    bookCount
       savedBooks {
         bookId
         authors
         image
-        description
         title
-        link
+        description
       }
     }
   }
